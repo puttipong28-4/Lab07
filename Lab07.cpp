335121d2fd4193e6e3894896c54a5686d7e93695
Puttipong Leekeawsai <118333377+puttipong28-4@users.noreply.github.com>
2023-02-10 19:42:57 +0700

    Update Lab07.cpp

--- Lab07.cpp
+++ Lab07.cpp
@@ -3,101 +3,148 @@
 
 struct studentNode
 {
-    char name[20];
-    int age;
-    char sex;
-    float gpa;
+  char name[20];
+  int age;
+  char sex;
+  float gpa;
+  struct studentNode *next;
+  struct studentNode *back;
 };
 
 class LinkedList
 {
 protected:
-    struct studentNode *start, **now;
+  struct studentNode *start, **now;
 
 public:
-    LinkedList();                                   
-    ~LinkedList();                                  
-    void InsNode(char n[], int a, char s, float g); 
-    void DelNode();                                 
-    GoNext();                                       
-    virtual void ShowNode();                        
-};// end class
+  LinkedList(); 
+  ~LinkedList(); 
+  void InsNode(char n[], int a, char s, float g);
+  void DelNode();
+  void GoNext(); 
+  virtual void ShowNode();
+}; // end class
 
 class NewList : public LinkedList
 {
 public:
-    GoFirst();              
-    virtual void ShowNode();
-};// end class
+  void GoFirst();          
+  virtual void ShowNode(); 
+}; // end class
 
 int main()
 {
-    LinkedList listA;
-    NewList listB;
-    LinkedList *listC;
+  LinkedList listA;
+  NewList listB;
+  LinkedList *listC = new NewList();
 
-    listA.InsNode("one", 1, 'A', 1.1);
-    listA.InsNode("two", 2, 'B', 2.2);
-    listA.InsNode("three", 3, 'C', 3.3);
-    listA.GoNext();
-    listA.ShowNode();
+  listA.InsNode("one", 1, 'A', 1.1);
+  listA.InsNode("two", 2, 'B', 2.2);
+  listA.InsNode("three", 3, 'C', 3.3);
+  listA.GoNext();
+  listA.ShowNode();
 
-    listB.InsertNode("four", 4, 'D', 4.4);
-    listB.InsertNode("five", 5, 'E', 5.5);
-    listB.InsertNode("six", 6, 'F', 6.6);
-    listB.GoNext();
-    listB.DelNode();
-    listB.ShowNode();
+  listB.InsNode("four", 4, 'D', 4.4);
+  listB.InsNode("five", 5, 'E', 5.5);
+  listB.InsNode("six", 6, 'F', 6.6);
+  listB.GoNext();
+  listB.DelNode();
+  listB.ShowNode();
 
-    listC = &listA;
-    listC->GoNext();
-    listC->ShowNode();
+  listC = &listA;
+  listC->GoNext();
+  listC->ShowNode();
 
-    listC = &listB;
-    listC->ShowNode();
+  listC = &listB;
+  listC->ShowNode();
 
-    return 0;
+  return 0;
+};
+
+void LinkedList ::InsNode(char n[], int a, char s, float g)
+{
+  struct studentNode *temp = new studentNode;
+  strcpy(temp->name, n);
+  temp->age = a;
+  temp->sex = s;
+  temp->gpa = g;
+
+  temp->next = start;
+  temp->back = NULL;
+
+  if (start != NULL)
+  {
+    start->back = temp;
+  }
+  start = temp;
+  now = &start;
 } // end function
 
-void LinkedList::InsNode( char n[], int a, char s, float g ) {
-  struct studentNode *temp = new studentNode ;
-  strcpy( temp->name, n ) ;
-  temp->age = a ;
-  temp->sex = s ;
-  temp->gpa = g ;
+void LinkedList ::GoNext()
+{
+  now = &(*now)->next;
+} // end function
 
-  temp->next = *now ;
-  *now = temp ;
-}
+void LinkedList ::ShowNode()
+{
+  printf("%s %d %c %f\n", (*now)->name, (*now)->age, (*now)->sex, (*now)->gpa);
+} // end function
 
-void LinkedList::DelNode() {
-  struct studentNode *temp = *now ;
-  *now = (*now)->next ;
-  delete temp ;
-}
-
-void LinkedList::GoNext() {
-  now = &((*now)->next) ;
-}
-
-void LinkedList::ShowNode() {
-  cout << "Name : " << (*now)->name << endl ;
-  cout << "Age : " << (*now)->age << endl ;
-  cout << "Sex : " << (*now)->sex << endl ;
-  cout << "GPA : " << (*now)->gpa << endl ;
-}
-
-void NewList::GoFirst() {
-  now = &start ;
-}
-
-void NewList::ShowNode() {
-  GoFirst() ;
-  while ( *now != NULL ) {
-    cout << "Name : " << (*now)->name << endl ;
-    cout << "Age : " << (*now)->age << endl ;
-    cout << "Sex : " << (*now)->sex << endl ;
-    cout << "GPA : " << (*now)->gpa << endl ;
-    GoNext() ;
+void NewList ::ShowNode()
+{
+  struct studentNode *temp;
+  if (start != NULL)
+  {
+    temp = start;
   }
+  else
+  {
+    start = temp;
+  }
+
+  while (start != NULL)
+  {
+    printf("%s ", start->name);
+    start = start->next;
+  } // end loop
+  printf("\n");
+
+} // end function
+
+void NewList ::GoFirst()
+{
+  now = &(start);
 }
+
+LinkedList ::LinkedList()
+{
+  start = NULL;
+}
+
+LinkedList ::~LinkedList()
+{
+  delete start;
+  delete now;
+}
+
+void LinkedList ::DelNode()
+{
+  if (*now == NULL)
+  {
+    return;
+  }
+
+  struct studentNode *temp = *now;
+  if ((*now)->back != NULL)
+  {
+    (*now)->back->next = (*now)->next;
+  }
+
+  if ((*now)->next != NULL)
+  {
+    (*now)->next->back = (*now)->back;
+    *now = (*now)->next;
+  }
+  delete temp;
+
+} // end function
